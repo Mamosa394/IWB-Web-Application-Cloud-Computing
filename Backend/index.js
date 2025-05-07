68a19c9b4210e20143cf9687db9c011a45327c96
Mamosa394 <motsiemamosa@gmail.com>
2025-05-06 21:15:04 -0700

    index

--- Backend/index.js
+++ Backend/index.js
@@ -15,10 +15,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use("/api/otp", otpRoutes);
 
 // MongoDB Connection
-mongoose.connect("mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<database>?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://tlouthabo07:tlouthabo@employment.sg91j.mongodb.net/TechStore?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
