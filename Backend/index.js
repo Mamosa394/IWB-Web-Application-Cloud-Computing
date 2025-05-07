27c19a07de477420c3721dde8849212689194bc1
Mamosa394 <motsiemamosa@gmail.com>
2025-05-06 21:08:29 -0700

    index

--- Backend/index.js
+++ Backend/index.js
@@ -18,11 +18,11 @@ app.use(express.json());
 app.use("/api/otp", otpRoutes);
 
 // MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect("mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<database>?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
